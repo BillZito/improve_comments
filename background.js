6f84c9169cb53037d61e22a2830c10b021d2aa22
Bill Zito <bill.r.zito@gmail.com>
2016-09-02 16:13:38 -0700

    Change HN posts with badwords to censored

---
+++ background.js
@@ -0,0 +1,10 @@
+//background.js
+
+//called when user clicks on browser action
+chrome.browserAction.onClicked.addListener(function(tab){
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+		var activeTab = tabs[0];
+		//sends message to current tab
+		chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+	});
+});
